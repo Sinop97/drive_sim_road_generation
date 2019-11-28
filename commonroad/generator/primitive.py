Fixed midline bleeding in curves, add instance ID pass for signs, moved all configuration to one location

commonroad/generator/primitive.py
@@ -95,15 +95,18 @@ class Primitive:
             else:
                 p1 = np.array(points[i])
 
-            left = p1 + ortho_left * (1 + 1E-8)
-            right = p1 + ortho_right * (1 + 1E-8)
+            left = p1 + ortho_left
+            right = p1 + ortho_right
 
+            precision_scale = 1E-2
             lanelet1.leftBoundary.point.append(
-                schema.point(x=points[i][0], y=points[i][1]))
+                schema.point(x=points[i][0] + ortho_left[0] * precision_scale,
+                             y=points[i][1] + ortho_left[1] * precision_scale))
             lanelet1.rightBoundary.point.append(
                 schema.point(x=right[0], y=right[1]))
             lanelet2.leftBoundary.point.append(
-                schema.point(x=points[i][0], y=points[i][1]))
+                schema.point(x=points[i][0] + ortho_right[0] * precision_scale,
+                             y=points[i][1] + ortho_right[1] * precision_scale))
             lanelet2.rightBoundary.point.append(
                 schema.point(x=left[0], y=left[1]))
         # TODO add last point

commonroad/renderer/ego_vehicle.py
@@ -86,19 +86,18 @@ def boundary_point_lengths(boundary):
         result.append(len)
     return result
 
-def boundary_to_equi_distant(boundary):
+def boundary_to_equi_distant(boundary, steps=20):
     lengths = boundary_point_lengths(boundary)
     x = list(map(lambda p: p.x, boundary.point))
     y = list(map(lambda p: p.y, boundary.point))
-    STEPS = 20
-    eval_marks = np.arange(0, lengths[-1], lengths[-1]/STEPS)
+    eval_marks = np.arange(0, lengths[-1], lengths[-1]/steps)
     xinterp = np.interp(eval_marks, lengths, x)
     yinterp = np.interp(eval_marks, lengths, y)
     return map(lambda i: (i[0],i[1]), zip(xinterp, yinterp))
 
-def middle_of_lanelet(lanelet):
-    left = boundary_to_equi_distant(lanelet.leftBoundary)
-    right = boundary_to_equi_distant(lanelet.rightBoundary)
+def middle_of_lanelet(lanelet, steps=20):
+    left = boundary_to_equi_distant(lanelet.leftBoundary, steps)
+    right = boundary_to_equi_distant(lanelet.rightBoundary, steps)
     return list(map(lambda p: ((p[0][0] + p[1][0])/2, (p[0][1] + p[1][1])/2),
         zip(left, right)))
 