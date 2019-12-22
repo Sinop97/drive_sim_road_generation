ed379f5e2816e78efa3ed9296430cd8585332e95
Mykyta Denysov <mykyta.denysov@bmw.de>
2019-12-22 19:30:40 +0100

    Adjust render default settings

--- blender-renderer.py
+++ blender-renderer.py
@@ -5,7 +5,8 @@ import shutil
 import json
 # because blender does not want to have it otherwise -__-
 sys.path.append(os.getcwd())
-sys.path += ['/usr/local/lib/python3.5/dist-packages', '/usr/lib/python3/dist-packages', '/usr/lib/python3.5/dist-packages']
+sys.path += ['/usr/local/lib/python3.5/dist-packages', '/usr/lib/python3/dist-packages',
+             '/usr/lib/python3.5/dist-packages']
 from blender.renderer.blender import generate_blend
 
 OUTPUT_DIR = 'blender-output-new'  # output directory
@@ -24,7 +25,7 @@ config = {'render_interval_distance': 0.05,
           # choose camera to render from, available: realsense, top
           # available: RGB (cycles render image), semseg_color (semantic segmentation colormap)
           # instances (id map of traffic signs (including poles)), lanes (DRIVABLE lane segmentation, only left/right)
-          'render_passes': ['rgb', 'semseg_color', 'instances'],
+          'render_passes': ['rgb', 'semseg_color', 'instances', 'lanes'],
           # resolution provided separately for each camera
           'frame_range': (0, -1),
           # use a .png to render the vehicle -> has to be re-generated for each camera position
@@ -40,7 +41,7 @@ config = {'render_interval_distance': 0.05,
                        'sensor_height': 5.32,  # 1/1.8 inch on IDS camera
                        'focal_length': 1.7,  # 1.7 mm on Theia
                        },
-                      # from: http://robotsforroboticists.com/wordpress/wp-content/uploads/2019/09/realsense-sep-2019.pdf
+                      # http://robotsforroboticists.com/wordpress/wp-content/uploads/2019/09/realsense-sep-2019.pdf
                       {'name': 'realsense',
                        'position_offset': {'x': -0.220317, 'y': 0.0325, 'z': 0.11},
                        # rotation of the cameras around the Y axis (lateral car axis) in degrees
@@ -50,7 +51,7 @@ config = {'render_interval_distance': 0.05,
                        'segmentation_mask': 'realsense_segmentation_mask.png',
                        'sensor_width': 6.4,  # 1/2 inch, see above for source
                        'sensor_height': 4.8,  # 1/2 inch, see above for source
-                       # 'focal_length': 1.93,  -> documentation above looks incorrect in this regard, use horizontal FOV
+                       # 'focal_length': 1.93,  -> documentation above incorrect, use horizontal FOV
                        'horizontal_fov': 69.4  # unit: degrees
                        }
                       ]
